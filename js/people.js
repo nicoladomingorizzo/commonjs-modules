b2fb0658666584f1be07e4a4708759f3f61af072
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-06-25 15:44:19 +0200

    Fix console log statements and update fullNameAndHobbies function to correctly call fullName and hobbies

--- js/people.js
+++ js/people.js
@@ -10,15 +10,15 @@ const fullName = require('./names.js');
 //Importa la tua funzione da hobbies.js
 const hobbies = require('./hobbies.js');
 
-//console.log(fullName('Nicola', 'Rizzo'))
-//console.log(hobbies('pallavolo', 'calcio', 'basket'));
+console.log(fullName('Nicola', 'Rizzo'))
+console.log(hobbies('pallavolo', 'calcio', 'basket'));
 
 //Crea una funzione che non ha parametri. La funzione dovrebbe restituire un oggetto con due proprietà: fullName ed hobbies. All'interno della tua funzione, usa le tue due funzioni precedenti per costruire l'oggetto.
 function fullNameAndHobbies() {
     return {
-        fullName: fullName(Nicola, Rizzo),
+        fullName: fullName('Nicola', 'Rizzo'),
         hobbies: hobbies('Calcio', 'Pokemon', 'Youtube')
     };
 };
 
-console.log(fullNameAndHobbies)
+console.log(fullNameAndHobbies());
